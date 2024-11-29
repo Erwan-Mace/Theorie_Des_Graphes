8bfa80bb8d873141dd952f3e69956fc1b6f8d7c4
BastienECE <150172707+BastienECE@users.noreply.github.com>
2024-11-29 16:16:42 +0100

    Update header.h

--- header.h
+++ header.h
@@ -20,6 +20,8 @@ struct Sommet
 {
     struct arc* arc;
     int valeur;
+    int N;
+    float coef;
     char *nom;
 
 };
@@ -36,8 +38,14 @@ typedef struct Graphe
 
 Graphe * lireFichier(char * nomFichier);
 void liberer_graphe(Graphe* graphe);
-//int bfs(Graphe* g, int source, int puits, int* parent);
+void creer_dot(const char *fichier_texte, const char *fichier_dot);
+void afficher_graphe_png(const char *fichier_dot);
 void liaisons(Graphe* graphe);
-
-
-#endif //CODE_BASE_PROJET_HEADER_H
+void Simu(Graphe* graphe);
+Graphe* Graphe_semaine(Graphe* graphe,int semaine);
+Graphe* Copie_graphe(Graphe* graphe_initial);
+void Successeur_Predecesseur(Graphe* graphe, int sommet, int* successeurs, int* nbSuccesseurs, int* predesseurs, int* nbPredesseurs);
+float SommePopMangee(int* predecesseurs, int nbPredesseurs, Graphe* graphe, float* populations);
+void Afficher_N(Graphe* graphe);
+void ecrireFichier(const Graphe* graphe, const char* nomFichier);
+#endif //CODE_BASE_PROJET
