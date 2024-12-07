099a8834b39663661b73d5ffcd3c32896a652abb
BastienECE <150172707+BastienECE@users.noreply.github.com>
2024-12-07 18:53:17 +0100

    Update header.h

--- header.h
+++ header.h
@@ -5,11 +5,13 @@
 #ifndef CODE_BASE_PROJET_HEADER_H
 #define CODE_BASE_PROJET_HEADER_H
 
-#define INF 10000
+#define  CoefMortalite 0.3
+#define Nmax 100000000
+#define  Kmin 0.0001
 //struct d'un arc
 struct arc
 {
-    int sommet; // numéro de sommet d'une arc adjacente au sommet initial
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
     float valeur;
     struct arc* arc_suivant;
 };
@@ -40,22 +42,21 @@ Graphe * lireFichier(char * nomFichier);
 void liberer_graphe(Graphe* graphe);
 void creer_dot(const char *fichier_texte, const char *fichier_dot);
 void afficher_graphe_png(const char *fichier_dot);
-void liaisons(Graphe* graphe);
 void Simu(Graphe* graphe);
 Graphe* Graphe_semaine(Graphe* graphe,int semaine);
 Graphe* Copie_graphe(Graphe* graphe_initial);
 void Successeur_Predecesseur(Graphe* graphe, int sommet, int* successeurs, int* nbSuccesseurs, int* predesseurs, int* nbPredesseurs);
-float SommePopMangee(int* predecesseurs, int nbPredesseurs, Graphe* graphe, float* populations);
+float SommePopMangee(int* successeurs, int nbSuccesseurs, Graphe* graphe, float* populations,int Sommetactuel);
 void Afficher_N(Graphe* graphe);
 void ecrireFichier(const Graphe* graphe, const char* nomFichier);
 void afficher_centre(const char* texte, int largeur_console);
 void dessiner_ligne(int largeur_console, char caractere);
 void afficher_titre_3D();
 void Analyse(Graphe* graphe);
-void unesource(Graphe* graphe);
-void liaisonniv(Graphe* graphe);
-int calculer_niveau(Graphe* graphe, int* niveaux, int index);
 void spredateurs(Graphe* graphe);
+void unesource(Graphe* graphe);
+int calculer_niveau(Graphe* graphe, int* niveaux, int index);
+void liaisonniv(Graphe* graphe);
 void centralite(Graphe* graphe);
 void connexite(Graphe* graphe);
 void lireFichier2(Graphe* graphe);
