6c8fb8bf7ad4aca13fb422205af8e1f1b7dd95d4
BastienECE <150172707+BastienECE@users.noreply.github.com>
2024-12-08 17:50:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,29 +7,27 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-    const int largeur_console = 148;
+
     Graphe* g = NULL;
     char nom_fichier[50];
     int choix;
 
     while (1) {
         printf("\033[0;35m");
-        // Afficher le titre en "3D"
-        dessiner_ligne(largeur_console, '=');
+        //affiche le titre en 3D
+        dessiner_ligne('=');
         afficher_titre_3D();
-        dessiner_ligne(largeur_console, '=');
+        dessiner_ligne('=');
 
-        // Menu principal
-        afficher_centre("Ecris un numero pour choisir ton graphe :", largeur_console);
+        afficher_centre("Ecris un numero pour choisir ton graphe :");
         printf("\n");
-        afficher_centre("1 : Mer", largeur_console);
-        afficher_centre("2 : Foret", largeur_console);
-        afficher_centre("3 : Savane", largeur_console);
-        afficher_centre("4 : ecosysteme sauvegarde", largeur_console);
-        afficher_centre("5 : Quitter", largeur_console);
+        afficher_centre("1 : Mer");
+        afficher_centre("2 : Foret");
+        afficher_centre("3 : Savane");
+        afficher_centre("4 : ecosysteme sauvegarde");
+        afficher_centre("5 : Quitter");
         printf("\n");
 
-        // Saisie du choix
         printf("Entrez votre choix : ");
         printf("\033[0m");
         scanf("%d", &choix);
@@ -39,7 +37,7 @@ int main() {
             break;
         }
 
-        // Traitement du choix
+        //swtich case des choix 
         switch (choix) {
             case 1:
                 printf("\nEcosysteme choisi : Mer\n");
@@ -63,7 +61,6 @@ int main() {
                 continue;
         }
 
-        // Lecture du graphe
         printf("\033[0m");
         g = lireFichier(nom_fichier);
         printf("\033[0;35m");
@@ -73,50 +70,48 @@ int main() {
         }
 
         while (1) {
-            // Menu des actions
+            //menu des choix
             printf("\033[0;35m");
-            dessiner_ligne(largeur_console, '-');
-            afficher_centre("Que voulez-vous faire ?", largeur_console);
-            dessiner_ligne(largeur_console, '-');
-            afficher_centre("1 : Lancer la simulation", largeur_console);
-            afficher_centre("2 : Analyser le graphe", largeur_console);
-            afficher_centre("3 : Revenir au menu principal", largeur_console);
+            dessiner_ligne('-');
+            afficher_centre("Que voulez-vous faire ?");
+            dessiner_ligne('-');
+            afficher_centre("1 : Lancer la simulation");
+            afficher_centre("2 : Analyser le graphe");
+            afficher_centre("3 : acceder aux Sources");
+            afficher_centre("4 : Revenir au menu principal");
             printf("\n");
 
-
-            // Saisie du choix
+            //saisie du choix + couleur
             printf("Entrez votre choix : ");
             printf("\033[0m");
             scanf("%d", &choix);
             printf("\033[0;35m");
             system("cls");
 
-            if (choix == 3) {
+            if (choix == 4) {
                 liberer_graphe(g);
                 g = NULL;
                 break;
             }
-
-            // Traitement du choix
             switch (choix) {
                 case 1:
-                    printf("\nSimulation\n");
+                    afficher_centre("Simulation");
+                    dessiner_ligne('-');
                     Simu(g);
                     break;
                 case 2:
-                    printf("\nAnalyse du graphe\n");
+                    afficher_centre("Analyse du graphe");
+                    dessiner_ligne('-');
                     Analyse(g);
                     break;
                 case 3:
-                    printf("\nSource\n");
-                    //source
+
+                    Source();
                 default:
                     printf("Cette option n'existe pas\n");
             }
         }
     }
-
-    // Libération de mémoire si nécessaire
     if (g) {
         liberer_graphe(g);
     }
